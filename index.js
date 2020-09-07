b791bad44af0957cbb53ab3af4989ba91228dadc
jmsilveira <jmsilveira@urudata.com>
2020-09-07 09:31:09 -0300

    initial configuration

---
+++ index.js
@@ -0,0 +1,17 @@
+const Koa = require('koa');
+const Router = require('@koa/router');
+const app = new Koa();
+const router = new Router;
+
+const PORT = process.env.PORT || 5000
+
+router.get('/', (ctx, next) => {
+    ctx.body = 'Hola mundo'
+});
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
+
+console.log('Listening on');
+app.listen(PORT);
